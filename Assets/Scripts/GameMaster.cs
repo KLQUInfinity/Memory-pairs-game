e283aa5fe0931e286b07117409821cbc2eb870bf
ahmed bendary <31450453+KLQUInfinity@users.noreply.github.com>
2019-08-09 17:05:06 +0200

    bendary

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMaster : MonoBehaviour
+{
+	private static GameMaster instance;
+
+	public static GameMaster Instance{ get { return instance; } }
+
+	private void Awake ()
+	{
+		if (instance == null) {
+			instance = this;
+			DontDestroyOnLoad (gameObject);
+		} else if (instance != this) {
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/SplashScreenManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SplashScreenManager : MonoBehaviour
+{
+    public Slider loadingSlider;
+
+    void Start()
+    {
+        StartCoroutine(Wait3Sec());
+    }
+
+    IEnumerator Wait3Sec()
+    {
+        yield return new WaitForSeconds(2);
+        loadingSlider.gameObject.SetActive(true);
+        AsyncOperation operation = SceneManager.LoadSceneAsync(1);
+        while (!operation.isDone)
+        {
+            loadingSlider.value = operation.progress;
+            yield return null;
+        }
+    }
+}
